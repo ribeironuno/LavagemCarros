b90df787d8390148a302eddb57485060b1de44f3
ribeironuno <ribeiro13nuno@gmail.com>
2021-11-24 14:57:27 +0000

    Alteração de rolos para thread

--- src/main/java/lavagem/Rolos.java
+++ src/main/java/lavagem/Rolos.java
@@ -1,11 +1,32 @@
 package lavagem;
 
-public class Rolos {
+import enumerations.EstadoRolos;
+
+import java.util.concurrent.Semaphore;
+
+public class Rolos implements Runnable {
+
+    /**
+     * Tipo de pedidos da main.
+     */
+    public enum PedidoMain {
+        LIGAR, SUSPENDER, RETOMAR;
+    }
+
+    /**
+     * Atual pedido da main.
+     */
+    private Rolos.PedidoMain pedidoMain;
 
     /**
      * Enumeração correspondente aos estados possiveis associados aos rolos
      */
     private EstadoRolos estado;
+
+    /**
+     * Semáforo que serve de comunicação entre main e rolos.
+     */
+    private Semaphore sem;
 
     /**
      * Duração em segundos do movimento dos rolos
@@ -15,24 +36,61 @@ public class Rolos {
     /**
      * Instancia os rolos com a duração enviada por parametero lida
      * atraves do ficheiro de configuração
-     *
-     * @param duracao duração em segundos da duração dos rolos
      */
-    public Rolos(int duracao) {
-        this.duracao = duracao;
+    public Rolos(Semaphore sem) {
+        this.sem = sem;
         this.estado = EstadoRolos.PARADO;
+        this.pedidoMain = null;
     }
 
-    public void setEstado(EstadoRolos estado) {
-        this.estado = estado;
+    /**
+     * Método que corresponde á ordem da main para com os rolos.
+     *
+     * @param duracao Tempo em segundos que os rolos irão funcionar.
+     */
+    public void ativarRolos(int duracao) {
+        this.duracao = duracao;
+        this.pedidoMain = PedidoMain.LIGAR;
     }
 
+    /**
+     * Obtem estado dos rolos.
+     *
+     * @return Retorna o estado atual dos rolos
+     */
     public EstadoRolos getEstado() {
         return this.estado;
     }
 
-    public int getDuracao() {
-        return this.duracao;
-    }
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            if (this.pedidoMain != null) {
+                switch (this.pedidoMain) {
+                    case LIGAR:
+                        this.estado = EstadoRolos.ATIVO; //Ativa os rolos
+                        try {
+                            Thread.sleep(this.duracao * 1000L); //Aguarda o tempo dado pela main
+                        } catch (InterruptedException ex) {
+                        }
+                        this.estado = EstadoRolos.PARADO; //Para rolos
+                        sem.release(); //E notifica main
+                        this.pedidoMain = null; //Reseta o pedido da main
+                        break;
+                    case SUSPENDER:
+                        break;
+                    case RETOMAR:
+                        break;
+                }
+            }
 
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) { //Verifica se foi interrompido e acaba
+                if (this.pedidoMain != PedidoMain.SUSPENDER) {
+                    return;
+                }
+            }
+        }
+    }
 }

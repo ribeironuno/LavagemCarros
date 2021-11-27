50fe09d6cc2106cd6cb2038045b7a1abf81052be
Josue Freitas <josue.natanael@hotmail.com>
2021-11-27 15:26:33 +0000

    Log comentado

--- src/main/java/lavagem/Log.java
+++ src/main/java/lavagem/Log.java
@@ -8,13 +8,18 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 import java.util.concurrent.Semaphore;
 
 public class Log implements Runnable {
 
+    /**
+     * Semaforo que faz a comunicação entre a main e a log
+     */
     private Semaphore sem;
 
+    /**
+     * Objecto partilhado utilizado para enviar a mensagem de escrita
+     */
     private SharedMainLog sharedObj;
 
     private File file;
@@ -23,9 +28,17 @@ public class Log implements Runnable {
 
     private PrintWriter pw;
 
+    /**
+     * Data utilizada no inicio de cada mensagem log
+     */
     private DateTimeFormatter date = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-    ;
 
+    /**
+     * Instancia Log com semaforo e objecto partilhado por parametro. Instancia biblioteca de escrita em ficheiro
+     * @param sem Semaforo que faz a comunicação entre a main e a log
+     * @param sharedObj Objecto partilhado utilizado para enviar a mensagem de escrita
+     * @throws IOException Se ocorrer algum erro na escrita de ficheiro
+     */
     public Log(Semaphore sem, SharedMainLog sharedObj) throws IOException {
         this.sem = sem;
         this.sharedObj = sharedObj;
--- src/main/java/sharedobjects/SharedMainLog.java
+++ src/main/java/sharedobjects/SharedMainLog.java
@@ -2,12 +2,23 @@ package sharedobjects;
 
 public class SharedMainLog {
 
+    /**
+     * Mensagem log a ser escrita para o txt
+     */
     private String message;
 
+    /**
+     * Define a mensagem a ser escrita no txt
+     * @param message mensagem a ser escrita
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * Retorna a mensagem a se escrita no txt
+     * @return mensagem a ser escrita
+     */
     public String getMessage() {
         return this.message;
     }
